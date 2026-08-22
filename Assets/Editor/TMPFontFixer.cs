bcff008cb0a0eb2311ede4e63512008d8804677d
a-i-am <sien6918@daum.net>
2026-08-22 17:27:34 +0900

    fix: update all TextMeshProUGUI components to use KoPubWorld Batang Bold asset
    - Ensure Button and FontTestText components render correctly with KoPubWorld font

--- Assets/Editor/TMPFontFixer.cs
+++ Assets/Editor/TMPFontFixer.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEditor;
 using TMPro;
-using System.IO;
 
 [InitializeOnLoad]
 public class TMPFontFixer
@@ -14,25 +13,12 @@ public class TMPFontFixer
     [MenuItem("Tools/Fix Fonts and Scene")]
     public static void FixFontsAndScene()
     {
-        string fontPath = "Assets/Resources/StudyGame-Art/Fonts/KoPubWorld Batang Bold.ttf";
-        Font font = AssetDatabase.LoadAssetAtPath<Font>(fontPath);
-        if (font == null) return;
+        string fontAssetPath = "Assets/Resources/StudyGame-Art/Fonts/KoPubWorld Batang Bold.asset";
+        TMP_FontAsset fontAsset = AssetDatabase.LoadAssetAtPath<TMP_FontAsset>(fontAssetPath);
+        if (fontAsset == null) return;
 
-        string assetPath = "Assets/Resources/StudyGame-Art/Fonts/KoPubWorld Batang Bold Dynamic.asset";
-        TMP_FontAsset fontAsset = AssetDatabase.LoadAssetAtPath<TMP_FontAsset>(assetPath);
-
-        if (fontAsset == null)
-        {
-            fontAsset = TMP_FontAsset.CreateFontAsset(font);
-            fontAsset.atlasPopulationMode = AtlasPopulationMode.Dynamic;
-            AssetDatabase.CreateAsset(fontAsset, assetPath);
-            AssetDatabase.SaveAssets();
-        }
-        else
-        {
-            fontAsset.atlasPopulationMode = AtlasPopulationMode.Dynamic;
-            EditorUtility.SetDirty(fontAsset);
-        }
+        fontAsset.atlasPopulationMode = AtlasPopulationMode.Dynamic;
+        EditorUtility.SetDirty(fontAsset);
 
         TextMeshProUGUI[] tmpTexts = Object.FindObjectsOfType<TextMeshProUGUI>(true);
         foreach (TextMeshProUGUI tmp in tmpTexts)
